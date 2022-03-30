aa1d0dfb177e0b897cac00f7d311bc5b941dc1fa
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-03-30 22:57:59 +0100

    Completed Pangram Finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,29 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
+  let result;
+  let lowerCasePhrase = this.phrase.toLowerCase();
+  let splitPhrase = lowerCasePhrase.split('');
+  splitPhrase.forEach((letter) => {
+      let letterIndex = this.alphabet.indexOf(letter);
+      if (this.alphabet.includes(letter)) {
+        this.alphabet.splice(letterIndex , 1)
+      }
+  });
 
-}
+  if (this.alphabet.length === 0){
+    result = true;
+  } else {
+    result = false;
+  }
+
+  // return result;
+  return result;
+
+};
 
 module.exports = PangramFinder;
+
